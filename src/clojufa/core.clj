f0de85580a4c25919c85ffe4ec967163fd2b07f8
Mungojelly <mungojelly@gmail.com>
2016-11-19 02:36:51 -0500

    uo sampu su'u kakne lo su'u gentufa do'e lo laldo
    .i milxe lo ka fegli kei fa lo tadji be lo su'u kakne lo su'u gentufa .i
    galfi lo .camxes. mapti lo .instaparse mapti ta'i lo rufsu pe'a .i ku'i
    si'au snada lo su'u kakne lo su'u gentufa co jbobo kei kei .u'a sai!

--- src/clojufa/core.clj
+++ src/clojufa/core.clj
@@ -1,6 +1,47 @@
-(ns clojufa.core)
+(ns clojufa.core
+  (:require [instaparse.core :as insta]
+            [clojure.string :refer [replace] :rename {replace basti}]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn tarvihu
+  "gasnu lo nu zo'oi *) noi .instaparse ke pinka sinxa cu co'u nenri"
+  [tarduhe]
+  (basti tarduhe "*)" "*\\)"))
+
+(defn sitnyvihu
+  "si'au na nelci zo'oi ' ji'a"
+  [sitnyduhe]
+  (basti sitnyduhe "'" "kuot"))
+
+(defn pinka-staile-galfi
+  "galfi lo laldo .peg. bo staile pinka lo .instaparse mapti"
+  [pinka]
+  (str "(* "
+       (sitnyvihu (tarvihu (pinka 1)))
+       " *)\n"))
+
+(defn camxes-galfi
+  "galfi lo .camxes.peg. lo se nelci be la'oi instaparse"
+  [pegli]
+  (-> pegli
+      (basti "<-" "=")
+      (basti "/" "|")
+      (basti #"#(.*)\n" pinka-staile-galfi)
+      (basti "[.\\t\\n\\r?!\\u0020]"
+             "( '.' | #'\\s+' )")
+      (basti #"\[(.*)\]" "#\"[$1]\"")
+      (basti ".*" "#\".*\"")
+      (basti "!." "!#\".\"")))
+
+(def camxes-peg
+  (-> "camxes.peg"
+      (clojure.java.io/resource)
+      (clojure.java.io/file)
+      (slurp)))
+
+(def camxes-gerna
+  (insta/parser (camxes-galfi camxes-peg)))
+
+(defn camxes
+  "gentufa do'e la .camxes."
+  [uenzi]
+  (apply camxes-gerna [uenzi]))
